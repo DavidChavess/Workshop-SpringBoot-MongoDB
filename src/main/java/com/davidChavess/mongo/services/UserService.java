3e5621711df47696e94d32c100b6477c94330f12
David Chaves <davi.ch.fe@gmail.com>
2020-05-15 11:37:13 -0300

    Get user by id

--- src/main/java/com/davidChavess/mongo/services/UserService.java
+++ src/main/java/com/davidChavess/mongo/services/UserService.java
@@ -1,23 +1,26 @@
 package com.davidChavess.mongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.davidChavess.mongo.domain.User;
 import com.davidChavess.mongo.repositories.UserRepository;
+import com.davidChavess.mongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 	
 	@Autowired
 	private UserRepository repository;
-	/*
+	
 	public User findById(String id) {
-		return null;
+		Optional<User> user = repository.findById(id);
+		return user.orElseThrow(()-> new ObjectNotFoundException("Object not found"));
 	}
-	*/
+	
 	public List<User> findAll(){
 		return repository.findAll();
 	}
